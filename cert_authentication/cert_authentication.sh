1fa07d1e3ccdc9d7b94f360cc40b14c0a52f16d8
mikail-eliyah-00 <eliyah.mikail@gmail.com>
2025-02-11 08:19:22 -0800

    .

---
+++ cert_authentication/cert_authentication.sh
@@ -0,0 +1,35 @@
+
+#1️⃣ Generate an RSA Key Pair
+openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
+openssl rsa -in private_key.pem -pubout -out public_key.pem
+
+#2️⃣ Create a Self-Signed Certificate
+openssl req -new -x509 -key private_key.pem -out certificate.pem -days 365 -subj "/C=US/ST=California/L=San Francisco/O=Example Corp/CN=example.com"
+#3️⃣ Sign a Nonce (Challenge)
+#echo "5a5a" > nonce.txt
+N=16  # Change this to the desired number of bytes
+openssl rand -hex $N > nonce.txt
+
+echo "nonce:"
+cat nonce.txt
+
+openssl dgst -sha256 -sign private_key.pem -out nonce.sig nonce.txt
+#4️⃣ Verify the Signature (Response)
+openssl dgst -sha256 -verify public_key.pem -signature nonce.sig nonce.txt
+
+
+echo ""
+: <<'NOTE_BLOCK_AREA'
+
+Generates an RSA key pair.
+Creates a self-signed certificate.
+Signs a nonce (5a5a) as a challenge.
+Verifies the signature using the public key.
+
+chmod +x ./cert_authentication.sh
+./cert_authentication.sh
+
+NOTE_BLOCK_AREA
+echo ""	
+
+
---
+++ cert_authentication/nonce.sig
@@ -0,0 +1,5 @@
+£u.óÞ%vUQ†»kS:Dä,‰5§Cêû·#ôo˜w~­ÖN¹´ys”\'‘Skô~¨ccD€ÕÌ'+šØëƒeÁ]Fö·°Â†L-°æŸºÖó­£,M´ª„þ‰1¢¡—áèk½îÐ%¢tOPôÍÄ®—Pè©^®Êˆû®4Þ­­C·×%¹æáY%`Åæ—[9­øHÌ
+‡MÌÔ¨i2Ø¶\þÙæok”ž<f+à+Þ½Áý!$RÞËè—P‚›Ï¯½
+ÓG’ï‚ôDN6ý$Fé$¿J>ô
+åcXÀ1ü±|bPßt¡L ;‡Ž¥#'Xôp8yÊ
+“
